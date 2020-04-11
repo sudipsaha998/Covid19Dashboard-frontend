b36df2237d6566185426d74fcef49607a15f838d
Sudip <sudipsaha998@gmail.com>
2020-04-11 16:11:50 +0530

    dashboard changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Covid19StatsComponent } from './covid19-stats/covid19-stats.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MycountrydoughnutComponent } from './mycountrydoughnut/mycountrydoughnut.component';
+import { WorldstatlineComponent } from './worldstatline/worldstatline.component';
+import { Covid19HomeComponent } from './covid19-home/covid19-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Covid19StatsComponent,
+    NavBarComponent,
+    MycountrydoughnutComponent,
+    WorldstatlineComponent,
+    Covid19HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
