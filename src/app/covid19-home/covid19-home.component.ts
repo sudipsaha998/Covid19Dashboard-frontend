763367b7a8057f6fee4ff8706e53205d3047f343
Sudip <sudipsaha998@gmail.com>
2020-04-12 23:30:13 +0530

    reverted user country data retrieve process

--- src/app/covid19-home/covid19-home.component.ts
+++ src/app/covid19-home/covid19-home.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { Covid19StatsComponent } from '../covid19-stats/covid19-stats.component';
 
 @Component({
   selector: 'app-covid19-home',
   templateUrl: './covid19-home.component.html',
   styleUrls: ['./covid19-home.component.css']
 })
-export class Covid19HomeComponent implements OnInit, AfterViewInit {
-
-  @ViewChild(Covid19StatsComponent, { static: false }) covid19Stat: Covid19StatsComponent;
-  countryDataSet: any;
+export class Covid19HomeComponent implements OnInit {
 
   constructor() { }
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.countryDataSet = this.covid19Stat.countries;
-    }, 1000)
-
-    console.log("after view init");
-  }
 
   ngOnInit() {
   }
--- src/app/mycountrydoughnut/mycountrydoughnut.component.ts
+++ src/app/mycountrydoughnut/mycountrydoughnut.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ChartType, ChartDataSets } from 'chart.js';
 import { MultiDataSet, Label, Color } from 'ng2-charts';
 import { Covid19StatsService } from 'src/services/covid19-stats.service';
 import { CountryISO3 } from 'src/assets/countries'
 import { IpLocationService } from 'src/services/ip-location.service';
+import { Covid19Affected } from 'src/models/covid19-response';
 
 @Component({
   selector: 'app-mycountrydoughnut',
   templateUrl: './mycountrydoughnut.component.html',
   styleUrls: ['./mycountrydoughnut.component.css']
 })
-export class MycountrydoughnutComponent implements OnInit, OnChanges {
-  @Input() countryDataSet: any;
+export class MycountrydoughnutComponent implements OnInit {
   yourCountry: string;
   yourCountryISO3: string;
   ipAddress: string;
@@ -26,41 +26,23 @@ export class MycountrydoughnutComponent implements OnInit, OnChanges {
 
   constructor(private covid19StatService: Covid19StatsService, private ipLocationService: IpLocationService) { }
 
-
-  ngOnChanges(): void {
+  ngOnInit() {
     this.getUserLocation();
   }
 
-  ngOnInit() {
-    //this.getUserLocation();
+  getYourCountryCovid19Cases(country: string): void {
+    this.covid19StatService.getYourCountryCovid19Cases(country)
+      .subscribe((res: Covid19Affected) => {
+        this.confirmed = res.confirmed;
+        this.recovered = res.recovered;
+        this.deaths = res.deaths;
+        this.updatedDataAvailable = true;
+        this.doughnutChartData = [
+          [this.confirmed, this.recovered, this.deaths]
+        ]
+      });
   }
-  /*
-    getYourCountryCovid19Cases(): void {
-      this.covid19StatService.getYourCountryCovid19Cases('IND')
-        .subscribe((res: Covid19Affected) => {
-          this.confirmed = res.confirmed;
-          this.recovered = res.recovered;
-          this.deaths = res.deaths;
-          this.updatedDataAvailable = true;
-          this.doughnutChartData = [
-            [this.confirmed, this.recovered, this.deaths]
-          ]
-        });
-    }
-    */
 
-  updateDoughnutChart(): void {
-    if (this.countryDataSet) {
-      this.confirmed = this.countryDataSet[this.yourCountryISO3] ? this.countryDataSet[this.yourCountryISO3].confirmed : 0;
-      this.recovered = this.countryDataSet[this.yourCountryISO3] ? this.countryDataSet[this.yourCountryISO3].recovered : 0;
-      this.deaths = this.countryDataSet[this.yourCountryISO3] ? this.countryDataSet[this.yourCountryISO3].deaths : 0;
-
-      this.doughnutChartData = [
-        [this.confirmed, this.recovered, this.deaths]
-      ];
-      this.updatedDataAvailable = true;
-    }
-  }
 
   getUserLocation(): void {
     this.ipLocationService.getIPAddress().subscribe((res: any) => {
@@ -68,13 +50,13 @@ export class MycountrydoughnutComponent implements OnInit, OnChanges {
       this.ipLocationService.getLocationFromIp(res.ip).subscribe((res: any) => {
         this.yourCountry = res.country_name;
         this.yourCountryISO3 = res.country_code_iso3;
-        this.updateDoughnutChart();
+        this.getYourCountryCovid19Cases(this.yourCountryISO3);
       });
     },
       error => {
         this.yourCountry = 'INDIA';
         this.yourCountryISO3 = 'IND';
-        this.updateDoughnutChart();
+        this.getYourCountryCovid19Cases(this.yourCountryISO3);
       });
   }
 
